87c5277879223e44a34c098b3292238beae4f6ea
Caro Viggiano <caroviggiano05@gmail.com>
2023-08-11 19:51:36 -0300

    a

--- js/carrito.js
+++ js/carrito.js
@@ -6,17 +6,27 @@ totalCompraSpan.textContent = totalCompra || "0";
 
 const metodosPagoContainer = document.getElementById("metodo-pago-container");
 
-const metodosPago = ["Efectivo", "Mercado Pago", "Crédito/Débito"];
+const metodosPago = [
+  { nombre: "Efectivo", preference_id: "efectivo_preference_id" },
+  { nombre: "Mercado Pago", preference_id: "mercadopago_preference_id" },
+  { nombre: "Crédito/Débito", preference_id: "tarjeta_preference_id" }
+];
+
 metodosPago.forEach((metodo, index) => {
   const button = document.createElement("button");
-  button.textContent = metodo;
+  button.textContent = metodo.nombre;
   button.addEventListener("click", () => {
-    Swal.fire(`Has seleccionado ${metodo}`, "Gracias por tu compra!", "success");
+    if (metodo.nombre === "Mercado Pago") {
+
+      const montoAPagar = totalCompra || 0;
+      window.location.href = `https://www.mercadopago.com.ar/home`;
+    } else {
+      Swal.fire(`Has seleccionado ${metodo.nombre}`, "Gracias por tu compra!", "success");
+    }
   });
 
   metodosPagoContainer.appendChild(button);
 });
-
 
 setTimeout(() => {
   Swal.fire({
@@ -25,4 +35,4 @@ setTimeout(() => {
     icon: "info",
     showConfirmButton: false
   });
-}, 0); 
+}, 0);
--- js/main.js
+++ js/main.js
@@ -6,23 +6,20 @@ class Producto {
   }
 }
 
-const productos = [
-  new Producto("Alfajor Fulbito de Mani", 50, "img/alfajor.webp"),
-  new Producto("Caramelo Butter Toffee", 15, "img/caramelos.jpg"),
-  new Producto("Gomitas ácidas Mogul", 345, "img/gomitas.webp"),
-  new Producto("Shot chocolate", 400, "img/chocolates.jpg"),
-  new Producto("Monster bebida energizante", 800, "img/bebidaenergizante.webp"),
-  new Producto("Agua", 250, "img/agua.webp"),
-  new Producto("Coca Cola", 750, "img/gaseosa.webp"),
-  new Producto("Aquarius", 520, "img/jugos.webp"),
-  new Producto("Marlboro", 710, "img/marlboro.png"),
-  new Producto("Camel", 810, "img/camel.jpg"),
-  new Producto("Chesterfield", 590, "img/chesterfield.jpg"),
-  new Producto("Lucky Strike", 720, "img/luckystrike.jpg")
- 
-];
+async function obtenerProductos() {
+  try {
+    const response = await fetch("http://localhost:3001/productos");
+    if (!response.ok) {
+      throw new Error("No se pudieron obtener los productos");
+    }
+    const productos = await response.json();
+    return productos.map(producto => new Producto(producto.nombre, producto.precio, producto.imagen));
+  } catch (error) {
+    throw new Error("Error al obtener los productos");
+  }
+}
 
-function mostrarProductos() {
+function mostrarProductos(productos) {
   const productosContainer = document.getElementById("productos-container");
 
   productos.forEach((producto, index) => {
@@ -95,6 +92,15 @@ function procesarCompra() {
 }
 
 const procesarCompraButton = document.getElementById("procesar-compra-button");
-procesarCompraButton.addEventListener("click", procesarCompra);
 
-mostrarProductos();
+async function cargarProductosYProcesarCompra() {
+  try {
+    const productos = await obtenerProductos();
+    mostrarProductos(productos);
+    procesarCompraButton.addEventListener("click", procesarCompra);
+  } catch (error) {
+    console.error("Error al cargar los productos:", error);
+  }
+}
+
+cargarProductosYProcesarCompra();
