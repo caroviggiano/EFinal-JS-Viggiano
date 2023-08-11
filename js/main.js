f280607f4ec0f60c4d0ff2b07cb914adcebb308b
Caro Viggiano <caroviggiano05@gmail.com>
2023-08-11 20:38:16 -0300

    b

--- js/main.js
+++ js/main.js
@@ -7,16 +7,27 @@ class Producto {
 }
 
 async function obtenerProductos() {
-  try {
-    const response = await fetch("http://localhost:3001/productos");
-    if (!response.ok) {
-      throw new Error("No se pudieron obtener los productos");
-    }
-    const productos = await response.json();
-    return productos.map(producto => new Producto(producto.nombre, producto.precio, producto.imagen));
-  } catch (error) {
-    throw new Error("Error al obtener los productos");
-  }
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const productos = [
+        new Producto("Alfajor Fulbito de Mani", 50, "img/alfajor.webp"),
+        new Producto("Caramelo Butter Toffee", 15, "img/caramelos.jpg"),
+        new Producto("Gomitas ácidas Mogul", 345, "img/gomitas.webp"),
+        new Producto("Shot chocolate", 400, "img/chocolates.jpg"),
+        new Producto("Monster bebida energizante", 800, "img/bebidaenergizante.webp"),
+        new Producto("Agua", 250, "img/agua.webp"),
+        new Producto("Coca Cola", 750, "img/gaseosa.webp"),
+        new Producto("Aquarius", 520, "img/jugos.webp"),
+        new Producto("Marlboro", 710, "img/marlboro.png"),
+        new Producto("Camel", 810, "img/camel.jpg"),
+        new Producto("Chesterfield", 590, "img/chesterfield.jpg"),
+        new Producto("Lucky Strike", 720, "img/luckystrike.jpg")
+            ];
+            
+      resolve(productos);
+    }, 1000); 
+  });
 }
 
 function mostrarProductos(productos) {
